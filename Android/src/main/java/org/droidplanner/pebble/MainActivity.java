ee41e9dc15261f1c8b5c976b476bfa6b7cbaf5a5
squilter <s.quilter@aol.com>
2015-01-04 12:49:51 -0500

    fix install_watchapp button

--- Android/src/main/java/org/droidplanner/pebble/MainActivity.java
+++ Android/src/main/java/org/droidplanner/pebble/MainActivity.java
@@ -1,15 +1,30 @@
 package org.droidplanner.pebble;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Toast;
+
+import com.getpebble.android.kit.PebbleKit;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -47,10 +62,6 @@ public class MainActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
@@ -58,4 +69,56 @@ public class MainActivity extends ActionBarActivity {
             return rootView;
         }
     }
+
+    public void installWatchapp(View view){
+        manualWatchappInstall();
+    }
+
+    /**
+     * Pebble Install Button. When clicked, will check for pebble if pebble is
+     * not present, error displayed. If it is, the pbw (pebble bundle) will be
+     * copied from assets to external memory (makes sure to overwrite), and
+     * sends pbw intent for pebble app to install bundle.
+     */
+    public boolean manualWatchappInstall() {
+        final Context context = getApplicationContext();
+        if (PebbleKit.isWatchConnected(context)) {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = context.getAssets().open("DroidPlanner.pbw");
+                File outFile = new File(Environment.getExternalStorageDirectory().getPath(),
+                        "DroidPlanner.pbw");
+                out = new FileOutputStream(outFile);
+                byte[] buffer = new byte[1024];
+                int read;
+                while ((read = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, read);
+                }
+                in.close();
+                in = null;
+                out.flush();
+                out.close();
+                out = null;
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.fromFile(outFile));
+                intent.setClassName("com.getpebble.android",
+                        "com.getpebble.android.ui.UpdateActivity");
+                startActivity(intent);
+            } catch (IOException e) {
+                Log.e("pebble", "Failed to copy pbw asset", e);
+                Toast.makeText(context, "Failed to copy pbw asset", Toast.LENGTH_SHORT)
+                        .show();
+            } catch (ActivityNotFoundException e) {
+                Log.e("pebble", "Pebble App Not installed", e);
+                Toast.makeText(context, "Pebble App Not installed", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        } else {
+            Toast.makeText(context, "No Pebble Connected", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
 }
