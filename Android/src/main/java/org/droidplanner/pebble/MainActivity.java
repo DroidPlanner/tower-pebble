deb097463a3b17f11a947da1e27f29bbf9a3f3cd
squilter <s.quilter@aol.com>
2015-01-08 19:34:42 -0500

    move offline install to new class

--- Android/src/main/java/org/droidplanner/pebble/MainActivity.java
+++ Android/src/main/java/org/droidplanner/pebble/MainActivity.java
@@ -1,33 +1,19 @@
 package org.droidplanner.pebble;
 
-import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.bluetooth.BluetoothProfile;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Toast;
 
-import com.getpebble.android.kit.PebbleKit;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import com.o3dr.android.client.interfaces.ServiceListener;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,54 +57,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void installWatchapp(View view){
-        manualWatchappInstall();
+        OfflineWatchappInstallUtil.manualWatchappInstall(getApplicationContext());
     }
-
-    /**
-     * Pebble Install Button. When clicked, will check for pebble if pebble is
-     * not present, error displayed. If it is, the pbw (pebble bundle) will be
-     * copied from assets to external memory (makes sure to overwrite), and
-     * sends pbw intent for pebble app to install bundle.
-     */
-    public boolean manualWatchappInstall() {
-        final Context context = getApplicationContext();
-        if (PebbleKit.isWatchConnected(context)) {
-            InputStream in = null;
-            OutputStream out = null;
-            try {
-                in = context.getAssets().open("DroidPlanner.pbw");
-                File outFile = new File(Environment.getExternalStorageDirectory().getPath(),
-                        "DroidPlanner.pbw");
-                out = new FileOutputStream(outFile);
-                byte[] buffer = new byte[1024];
-                int read;
-                while ((read = in.read(buffer)) != -1) {
-                    out.write(buffer, 0, read);
-                }
-                in.close();
-                in = null;
-                out.flush();
-                out.close();
-                out = null;
-
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.fromFile(outFile));
-                intent.setClassName("com.getpebble.android",
-                        "com.getpebble.android.ui.UpdateActivity");
-                startActivity(intent);
-            } catch (IOException e) {
-                Log.e("pebble", "Failed to copy pbw asset", e);
-                Toast.makeText(context, "Failed to copy pbw asset", Toast.LENGTH_SHORT)
-                        .show();
-            } catch (ActivityNotFoundException e) {
-                Log.e("pebble", "Pebble App Not installed", e);
-                Toast.makeText(context, "Pebble App Not installed", Toast.LENGTH_SHORT)
-                        .show();
-            }
-        } else {
-            Toast.makeText(context, "No Pebble Connected", Toast.LENGTH_SHORT).show();
-        }
-        return true;
-    }
-
 }
---
+++ Android/src/main/java/org/droidplanner/pebble/OfflineWatchappInstallUtil.java
@@ -0,0 +1,66 @@
+package org.droidplanner.pebble;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.getpebble.android.kit.PebbleKit;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class OfflineWatchappInstallUtil {
+
+    /**
+     * Pebble Install Button. When clicked, will check for pebble if pebble is
+     * not present, error displayed. If it is, the pbw (pebble bundle) will be
+     * copied from assets to external memory (makes sure to overwrite), and
+     * sends pbw intent for pebble app to install bundle.
+     */
+    public static boolean manualWatchappInstall(Context context) {
+        if (PebbleKit.isWatchConnected(context)) {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = context.getAssets().open("DroidPlanner.pbw");
+                File outFile = new File(Environment.getExternalStorageDirectory().getPath(),
+                        "DroidPlanner.pbw");
+                out = new FileOutputStream(outFile);
+                byte[] buffer = new byte[1024];
+                int read;
+                while ((read = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, read);
+                }
+                in.close();
+                in = null;
+                out.flush();
+                out.close();
+                out = null;
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.fromFile(outFile));
+                intent.setClassName("com.getpebble.android",
+                        "com.getpebble.android.ui.UpdateActivity");
+                context.startActivity(intent);
+            } catch (IOException e) {
+                Log.e("pebble", "Failed to copy pbw asset", e);
+                Toast.makeText(context, "Failed to copy pbw asset", Toast.LENGTH_SHORT)
+                        .show();
+            } catch (ActivityNotFoundException e) {
+                Log.e("pebble", "Pebble App Not installed", e);
+                Toast.makeText(context, "Pebble App Not installed", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        } else {
+            Toast.makeText(context, "No Pebble Connected", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+}
